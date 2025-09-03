405a689a2a38fb9a27679d3d8e5263414df663a2
FerFerZ1985 <fernandosebastianzarate1985@gmail.com>
2025-09-03 19:52:45 -0300

    cambios en js

--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,15 @@
-// function main () {
-//     alert ('Aca te vas a enterar todo sobre Crossfit')
-// }
-
 let namesAndSurnames = "Carlos Soto"
-var x = 10;
+var x = '2022'
+let precioproducto = 0;
+var esMayorDeEdad = true;
+let persona = [20,'Juan Perez',true, 1,68]
+const PROGRAM_NAME = "APLICACION DE LOGICA DE PROGRAMACION"
+
+var tav = document.getElementById('output');
+tav.innerHTML = x;
+
+function changeVariable() {
+    x = prompt ('Dame el nuevo valor');
+    tav.innerHTML = x;
+}
+
